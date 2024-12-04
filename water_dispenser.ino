cf6d786160e64098cbc2f00653613be8b3366bea
Niharika Patil <nihapatil38@gmail.com>
2024-12-04 15:20:35 -0600

    Button working

--- water_dispenser.ino
+++ water_dispenser.ino
@@ -8,17 +8,40 @@
 //#include  <I2CLiquidCrystal.h>
 
 #define waterpump 8
+int buttonPin = 5;
+int button_state;
 
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
-    pinMode(waterpump, OUTPUT);
-    digitalWrite(waterpump, LOW);
-
+  Serial.begin(9600);
+  pinMode(waterpump, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  digitalWrite(waterpump, LOW);
 }
 
 void  loop() {
-  // if (soilMoisture > 650) {
-    digitalWrite(waterpump, HIGH);  // Turn on water pump
-  //}
-  
+  int reading = digitalRead(buttonPin);
+  Serial.println(reading);
+  if (reading != lastButtonState) { //this will check for if we are using the first button
+    //Serial.print("HELLO\n");
+    lastDebounceTime = millis(); //reset the debounce timer
+  }
+  Serial.print("HELLO IAM AHERE\n");
+  if ((millis() - lastDebounceTime) > debounceDelay) { 
+    if (reading != button_state){
+      button_state = reading;
+      if (button_state == HIGH) {
+        digitalWrite(waterpump, HIGH);  // Turn on water pump
+        Serial.print("button is high\n");
+      }
+      else {
+        digitalWrite(waterpump, LOW);
+        Serial.print("button is low\n");
+      }
+    }
+  } 
+  lastButtonState = reading;
 }
