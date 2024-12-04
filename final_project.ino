d77a4f3b770cfb1060b6b0d7fe91b4aafcb853ad
kavyaC510 <130185776+kavyaC510@users.noreply.github.com>
2024-12-04 00:54:09 -0600

    Add files via upload

---
+++ final_project.ino
@@ -0,0 +1,77 @@
+// Kavyasri Challa, 653418742, kchal3
+// Lab 4 - Analog Input: Photoresistor (LDR – Light Dependent Resistor)
+// This code is supposed take the inputs from the photo resistor and assign 5 predefined values(dark, partially dark, medium, fully lit, brightly lit). The predefined values will be displayed on the first row of the lcd based on the value that the photoresistor is reading and the seconds will be displayed on the second row of the lcd.
+// I thought that the photoresitor would have a very high value such as 1000 for brightly lit.
+// References: : https://www.circuitbasics.com/how-to-usephotoresistors-to-detect-light-on-an-arduino/ - provided in the lab description
+// TA Name: Yamaan Nandolia
+
+// #include <LiquidCrystal_I2C.h>
+// #include  <Wire.h>
+// LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#include <Adafruit_NeoPixel.h>
+
+#define LED_COUNT 10
+#define Pin 4
+
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(LED_COUNT, Pin, NEO_GRB + NEO_KHZ800);
+
+int R = 255;
+int G = 100;
+int B = 150;
+int brightness = 32;
+
+int photoPin = A0; // This is the pin that resistor is connected in the arduino
+unsigned long previousMillis = 0; 
+int timedelay = 500; // This the interval for which the photoresistor should read the value
+
+// The below sets up the lcd screen and sets the cursor to 0,0 and the photo resistor
+void setup() {
+  // lcd.init();
+  // lcd.backlight();
+  // lcd.setCursor(0,0);
+  leds.begin();
+  clearLEDs();
+  Serial.begin(9600);
+  pinMode(photoPin,INPUT);
+}
+
+void loop() {
+
+  // for(int i = 0; i < LED_COUNT; i++){
+  //   leds.setPixelColor(i,R,G,B);
+  //   leds.setBrightness(brightness);
+  //   leds.show();
+  // }
+
+
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= timedelay){
+    // lcd.clear(); // This clears the lcd display everytime before it displays the values
+    // lcd.setCursor(0,0);
+    int light = analogRead(photoPin);
+    Serial.println(light);
+    previousMillis = currentMillis;
+    // The below if statements print the 5 predefined values on the lcd's based on the value of the photo resistor
+    if(light < 350){
+      for(int i = 0; i < LED_COUNT; i++){
+        leds.setPixelColor(i,R,G,B);
+        leds.setBrightness(brightness);
+        leds.show();
+      }
+    }
+    else{
+      clearLEDs();
+    }
+    // The below 2 lines prints milliseconds in the second row of the lcd
+    // lcd.setCursor(0,1);
+    // lcd.print(currentMillis);
+  }
+}
+
+void clearLEDs(){
+  for(int i = 0; i < 30; i++){
+    leds.setPixelColor(i,0);
+    leds.show();
+  }
+}
